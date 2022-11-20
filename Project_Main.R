87cea66e0843d979199f1982efcb93bfd8657200
ballingsclass <59874436+ballingsclass@users.noreply.github.com>
2022-11-20 08:09:05 -0500

    initial commit

---
+++ Project_Main.R
@@ -0,0 +1,8 @@
+# BZAN 542 Project
+
+setwd("C:/Users/abdul/Desktop/UTK Documents/Classes/Data Mining for Business/BZAN542_FinalProject")
+
+#importing dataset
+
+df = read.csv("telecom_customer_churn.csv")
+View(df)
