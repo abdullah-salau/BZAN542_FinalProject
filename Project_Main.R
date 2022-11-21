8ceb42bd4a370bd6d49ddd95685c09d5917ce82f
ballingsclass <59874436+ballingsclass@users.noreply.github.com>
2022-11-20 19:40:24 -0500

    Update Project_Main.R

--- Project_Main.R
+++ Project_Main.R
@@ -6,3 +6,13 @@ setwd("C:/Users/abdul/Desktop/UTK Documents/Classes/Data Mining for Business/BZA
 
 df = read.csv("telecom_customer_churn.csv")
 View(df)
+
+#Subsetting dataset to only include churned or stayed customers
+df = df["|"(df$Customer.Status=='Churned',df$Customer.Status=='Stayed'),]
+
+#Analyze missing values
+summary(df)
+table(is.na(df))
+sapply(df, function(x) sum(is.na(x))/length(x))*100
+
+#Columns with NA values: Avg.Monthly.Long.Distance.Charges and Avg.Monthly.GB.Download
